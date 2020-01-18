991626617296fd721752775703c5ff579b006e41
MegaThorx <megathorx@gmail.com>
2020-01-18 22:59:53 +0100

    Rework vehicle tile view

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -33,28 +33,12 @@
                 <div class="row">
                     @foreach($group->vehicles as $vehicle)
                         <div class="col-md-4">
-                            <div class="card">
-                                <img class="bd-placeholder-img card-img-top" src="https://exo-reallife.de/images/veh/Vehicle_{{ $vehicle->Model }}.jpg">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $vehicle->getName() }}</h5>
-                                    <dl class="vehicle-info">
-                                        <dt>Kilometerstand</dt>
-                                        <dd>{{ number_format($vehicle->Mileage / 1000, 2, ',', ' ') }} km</dd>
-                                        <dt>Lackfarbe</dt>
-                                        <dd class="d-flex">
-                                            <div class="border" style="height: 25px; width: 25px; background-color: {{ $vehicle->getTuningColor(1) }};"></div>
-                                            <div class="border" style="height: 25px; width: 25px; background-color: {{ $vehicle->getTuningColor(2) }};"></div>
-                                            <div class="border" style="height: 25px; width: 25px; background-color: {{ $vehicle->getTuningColor(3) }};"></div>
-                                            <div class="border" style="height: 25px; width: 25px; background-color: {{ $vehicle->getTuningColor(4) }};"></div>
-                                        </dd>
-                                    </dl>
-                                </div>
-                            </div>
+                            @include('partials.vehicle')
                         </div>
                     @endforeach
                 </div>
+                @endcan
             </div>
-            @endcan
             <div class="col-lg-6">
                 @can('activityTotal', $group)
                     <react-chart data-chart="group:{{ $group->Id }}" data-state="true" data-title="{{ __('Aktivität') }}"></react-chart>
