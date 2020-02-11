b132ae4b4203686ae8be627732f15f46f12cfb33
MegaThorx <megathorx@gmail.com>
2020-02-11 20:22:00 +0100

    Fix permissions

--- app/Policies/VehiclePolicy.php
+++ app/Policies/VehiclePolicy.php
@@ -23,7 +23,7 @@ class VehiclePolicy
     public function before($user, $ability)
     {
         if ($user->Rank >= 2) {
-            // return true;
+            return true;
         }
     }
 
@@ -34,6 +34,16 @@ class VehiclePolicy
 
     public function private(User $user, Vehicle $vehicle)
     {
-        return true;
+        if($vehicle->OwnerType === 1) {
+            return $user->Id == $vehicle->OwnerId;
+        } elseif($vehicle->OwnerType === 2) {
+            return $user->FactionId == $vehicle->OwnerId;
+        } elseif($vehicle->OwnerType === 3) {
+            return $user->CompanyId == $vehicle->OwnerId;
+        } elseif($vehicle->OwnerType === 4) {
+            return $user->GroupId == $vehicle->OwnerId;
+        }
+
+        return false;
     }
 }
