192ad6ea80c1108f64b5abe9aa7b2a009972ca99
MegaThorx <megathorx@gmail.com>
2020-02-11 20:16:45 +0100

    Fix bug with getGroupName

--- app/Character.php
+++ app/Character.php
@@ -74,7 +74,7 @@ class Character extends Model
 
     public function getGroupName()
     {
-        if ($this->GroupId === 0)
+        if ($this->GroupId === 0 && $this->group)
             return 'keine';
         return $this->group->Name;
     }
