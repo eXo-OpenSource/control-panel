5987d95c0db23fd32b8f7757e3c906a9b0713568
MegaThorx <git@merx.dev>
2020-05-10 19:57:18 +0200

    Improve vehicles view

--- app/Http/Controllers/Admin/VehicleController.php
+++ app/Http/Controllers/Admin/VehicleController.php
@@ -12,7 +12,52 @@ class VehicleController extends Controller
     {
         abort_unless(Gate::allows('admin-rank-3'), 403);
 
-        $vehicles = DB::select('SELECT Model, COUNT(Id) AS Count FROM vrp_vehicles GROUP BY Model ORDER BY Model');
+        $data = DB::select('SELECT   v.Model,
+                                            IF(v.Premium > 0, true, false) AS Premium,
+                                            COUNT(v.Id) AS Count
+                                        FROM vrp_vehicles v
+                                        WHERE (v.OwnerType = 1 OR v.OwnerType = 4) AND v.Deleted IS NULL
+                                        GROUP BY IF(v.Premium > 0, true, false), v.Model
+                                        ORDER BY v.Model');
+
+        $shop = DB::select('SELECT Model FROM vrp_vehicle_shop_veh');
+
+        $vehicles = [];
+
+        foreach(config('constants.vehicleNames') as $vehicleId => $vehicleName)
+        {
+            $vehicles[$vehicleId] = (object)[
+                'Id' => $vehicleId,
+                'Name' => $vehicleName,
+                'Count' => 0,
+                'TradeAbleCount' => 0,
+                'PremiumCount' => 0,
+                'IsInShop' => false
+            ];
+
+            foreach($shop as $shopVehicle) {
+                if($shopVehicle->Model === $vehicleId) {
+                    $vehicles[$vehicleId]->IsInShop = true;
+                    break;
+                }
+            }
+        }
+
+
+        foreach($data as $vehicle)
+        {
+            if($vehicle->Premium) {
+                $vehicles[$vehicle->Model]->PremiumCount = $vehicle->Count;
+                $vehicles[$vehicle->Model]->Count = $vehicles[$vehicle->Model]->Count + $vehicle->Count;
+            } else {
+                $vehicles[$vehicle->Model]->TradeAbleCount = $vehicle->Count;
+                $vehicles[$vehicle->Model]->Count = $vehicles[$vehicle->Model]->Count + $vehicle->Count;
+            }
+        }
+
+        usort($vehicles, function($a, $b) {
+           return $a->Count < $b->Count;
+        });
 
         return view('admin.vehicles.index', compact('vehicles'));
     }
--- resources/views/admin/vehicles/index.blade.php
+++ resources/views/admin/vehicles/index.blade.php
@@ -8,12 +8,16 @@
                     @foreach($vehicles as $vehicle)
                         <div class="col-md-2">
                             <div class="card">
-                                <img class="bd-placeholder-img card-img-top" src="https://exo-reallife.de/images/veh/Vehicle_{{ $vehicle->Model }}.jpg">
+                                <img class="bd-placeholder-img card-img-top" src="https://exo-reallife.de/images/veh/Vehicle_{{ $vehicle->Id }}.jpg">
                                 <div class="card-body">
-                                    <h5 class="card-title">@vehicleName($vehicle->Model)</h5>
+                                    <h5 class="card-title">{{ $vehicle->Name }}@if(!$vehicle->IsInShop) <span class="badge badge-info">{{ __('Selten') }}</span>@endif</h5>
                                     <dl class="vehicle-info">
                                         <dt>{{ __('Anzahl') }}</dt>
                                         <dd>{{ $vehicle->Count }}</dd>
+                                        <dt>{{ __('Handelbar') }}</dt>
+                                        <dd>{{ $vehicle->TradeAbleCount }}</dd>
+                                        <dt>{{ __('nicht Handelbar') }}</dt>
+                                        <dd>{{ $vehicle->PremiumCount }}</dd>
                                     </dl>
                                 </div>
                             </div>
