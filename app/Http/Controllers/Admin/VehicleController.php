d60cd3ce7ea990094e82135872c6d56c44a3e8ae
MegaThorx <git@merx.dev>
2020-05-11 12:07:11 +0200

    Rework vehicle view

--- app/Http/Controllers/Admin/VehicleController.php
+++ app/Http/Controllers/Admin/VehicleController.php
@@ -13,11 +13,12 @@ class VehicleController extends Controller
         abort_unless(Gate::allows('admin-rank-3'), 403);
 
         $data = DB::select('SELECT   v.Model,
+                                            v.OwnerType,
                                             IF(v.Premium > 0, true, false) AS Premium,
                                             COUNT(v.Id) AS Count
                                         FROM vrp_vehicles v
-                                        WHERE (v.OwnerType = 1 OR v.OwnerType = 4) AND v.Deleted IS NULL
-                                        GROUP BY IF(v.Premium > 0, true, false), v.Model
+                                        WHERE v.Deleted IS NULL
+                                        GROUP BY v.OwnerType, IF(v.Premium > 0, true, false), v.Model
                                         ORDER BY v.Model');
 
         $shop = DB::select('SELECT Model FROM vrp_vehicle_shop_veh');
@@ -30,6 +31,10 @@ class VehicleController extends Controller
                 'Id' => $vehicleId,
                 'Name' => $vehicleName,
                 'Count' => 0,
+                'PlayerOwned' => 0,
+                'FactionOwned' => 0,
+                'CompanyOwned' => 0,
+                'GroupOwned' => 0,
                 'TradeAbleCount' => 0,
                 'PremiumCount' => 0,
                 'IsInShop' => false
@@ -46,12 +51,31 @@ class VehicleController extends Controller
 
         foreach($data as $vehicle)
         {
-            if($vehicle->Premium) {
-                $vehicles[$vehicle->Model]->PremiumCount = $vehicle->Count;
-                $vehicles[$vehicle->Model]->Count = $vehicles[$vehicle->Model]->Count + $vehicle->Count;
-            } else {
-                $vehicles[$vehicle->Model]->TradeAbleCount = $vehicle->Count;
-                $vehicles[$vehicle->Model]->Count = $vehicles[$vehicle->Model]->Count + $vehicle->Count;
+            switch($vehicle->OwnerType)
+            {
+                case 1:
+                    $vehicles[$vehicle->Model]->PlayerOwned = $vehicles[$vehicle->Model]->PlayerOwned + $vehicle->Count;
+                    break;
+                case 2:
+                    $vehicles[$vehicle->Model]->FactionOwned = $vehicles[$vehicle->Model]->FactionOwned + $vehicle->Count;
+                    break;
+                case 3:
+                    $vehicles[$vehicle->Model]->CompanyOwned = $vehicles[$vehicle->Model]->CompanyOwned + $vehicle->Count;
+                    break;
+                case 4:
+                    $vehicles[$vehicle->Model]->GroupOwned = $vehicles[$vehicle->Model]->GroupOwned + $vehicle->Count;
+                    break;
+            }
+
+
+            if($vehicle->OwnerType === 1 || $vehicle->OwnerType === 4) {
+                if($vehicle->Premium) {
+                    $vehicles[$vehicle->Model]->PremiumCount = $vehicle->Count;
+                    $vehicles[$vehicle->Model]->Count = $vehicles[$vehicle->Model]->Count + $vehicle->Count;
+                } else {
+                    $vehicles[$vehicle->Model]->TradeAbleCount = $vehicle->Count;
+                    $vehicles[$vehicle->Model]->Count = $vehicles[$vehicle->Model]->Count + $vehicle->Count;
+                }
             }
         }
 
--- resources/views/admin/vehicles/index.blade.php
+++ resources/views/admin/vehicles/index.blade.php
@@ -8,17 +8,28 @@
                     @foreach($vehicles as $vehicle)
                         <div class="col-md-2">
                             <div class="card">
+                                @if(!$vehicle->IsInShop)<span class="image-badge image-badge-info">{{ __('Selten') }}</span>@endif
                                 <img class="bd-placeholder-img card-img-top" src="https://exo-reallife.de/images/veh/Vehicle_{{ $vehicle->Id }}.jpg">
                                 <div class="card-body">
-                                    <h5 class="card-title">{{ $vehicle->Name }}@if(!$vehicle->IsInShop) <span class="badge badge-info">{{ __('Selten') }}</span>@endif</h5>
-                                    <dl class="vehicle-info">
-                                        <dt>{{ __('Anzahl') }}</dt>
-                                        <dd>{{ $vehicle->Count }}</dd>
-                                        <dt>{{ __('Handelbar') }}</dt>
-                                        <dd>{{ $vehicle->TradeAbleCount }}</dd>
-                                        <dt>{{ __('nicht Handelbar') }}</dt>
-                                        <dd>{{ $vehicle->PremiumCount }}</dd>
-                                    </dl>
+                                    <div class="row">
+                                        <div class="col-12 d-flex justify-content-between align-items-center">
+                                            <span class="card-title h5">{{ $vehicle->Name }}</span>
+                                            <button type="button" class="btn btn-light float-right" data-toggle="tooltip" data-html="true"
+                                                    data-placement="left" data-animation="true" data-original-title="
+                                                {{ __('Spielerbesitz: ') }} {{ $vehicle->PlayerOwned }}<br>
+                                                {{ __('Fraktionsbesitz: ') }} {{ $vehicle->FactionOwned }}<br>
+                                                {{ __('Unternehmensbesitz: ') }} {{ $vehicle->CompanyOwned }}<br>
+                                                {{ __('Gruppenbesitz: ') }} {{ $vehicle->GroupOwned }}<br>">
+                                                <i class="fas fa-info-circle"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <span class="d-block">{{ __('Gesamt: ') }} {{ $vehicle->Count }}</span>
+                                            <span>{{ __('Handelbar: ') }} {{ $vehicle->TradeAbleCount }}</span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -29,3 +40,11 @@
     </div>
 @endsection
 
+@section('script')
+    <script>
+        document.querySelectorAll('[data-toggle="tooltip"]').forEach(function (element) {
+            // eslint-disable-next-line no-new
+            new coreui.Tooltip(element);
+        });
+    </script>
+@endsection
