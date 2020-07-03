2b7d3a97c668a706c93824c6e9c9d46d59f2987d
MegaThorx <git@merx.dev>
2020-07-03 14:58:45 +0200

    Rework commits page layout

--- resources/views/commits/index.blade.php
+++ resources/views/commits/index.blade.php
@@ -14,19 +14,25 @@
 
                 <p class="h3 mb-4">{{ __('Letzten 100 Commits vom') }} {{ $allowedProjects[$selectedProject] }}</p>
                 @foreach($commits as $commit)
-                    <div class="card">
-                        <div class="card-body">
+                    <div class="card mb-3">
+                        <div class="card-body p-2">
                             <div class="row">
                                 <div class="col-2 text-center">
                                     <img src="{{ $commit['avatar'] }}" style="width: 50px; height: 50px;" class="rounded" />
-                                    <p>{{ $commit['author'] }}</p>
+                                    <span class="d-block">{{ $commit['author'] }}</span>
+
                                 </div>
                                 <div class="col-10">
-                                    <span class="float-right text-right">
-                                        <span class="d-block">{{ \Carbon\Carbon::parse($commit['date'])->format('d.m.Y H:m') }}</span>
-                                        <span class="badge badge-secondary">{{ $commit['branch'] }}</span>
-                                    </span>
-                                    {{ $commit['commit'] }}
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <span class="badge badge-secondary">{{ $commit['branch'] }}</span> <small>{{ 'am' }} {{ \Carbon\Carbon::parse($commit['date'])->format('d.m.Y H:m') }}</small>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            {{ $commit['commit'] }}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
