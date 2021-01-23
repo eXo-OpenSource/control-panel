c3f990bfb53dd22a8d4e86e7077fabbb436a9d93
MegaThorx <git@merx.dev>
2021-01-23 17:39:07 +0100

    Addendum last commit

--- app/Http/Controllers/Admin/Api/TicketController.php
+++ app/Http/Controllers/Admin/Api/TicketController.php
@@ -21,14 +21,29 @@ class TicketController extends Controller
         $weeks = 25;
 
         $members = User::query()->where('Rank', '>', 0)->get();
-        $stats = DB::select('SELECT WEEK(CreatedAt) + 1 AS Week, AssigneeId, COUNT(Id) AS Count FROM vrp_tickets WHERE AssigneeId IS NOT NULL AND State = \'Closed\' AND CreatedAt >= DATE_SUB(SUBDATE(NOW(), WEEKDAY(NOW())), INTERVAL ? WEEK) GROUP BY Week, AssigneeId;', [$weeks]);
-        $stats2 = DB::select('SELECT WEEK(t.CreatedAt) + 1 AS Week, tu.UserId, COUNT(tu.UserId) AS Count FROM vrp_ticket_users tu INNER JOIN vrp_tickets t ON t.Id = tu.TicketId WHERE t.AssigneeId IS NOT NULL AND t.AssigneeId <> tu.UserId AND t.State = \'Closed\' AND t.CreatedAt >= DATE_SUB(SUBDATE(NOW(), WEEKDAY(NOW())), INTERVAL ? WEEK) AND tu.IsAdmin = 1 AND tu.LeftAt IS NULL GROUP BY Week, UserId;', [$weeks]);
+        // $stats = DB::select('SELECT WEEK(CreatedAt) + 1 AS Week, AssigneeId, COUNT(Id) AS Count FROM vrp_tickets WHERE AssigneeId IS NOT NULL AND State = \'Closed\' AND CreatedAt >= DATE_SUB(SUBDATE(NOW(), WEEKDAY(NOW())), INTERVAL ? WEEK) GROUP BY Week, AssigneeId;', [$weeks]);
+        // $stats2 = DB::select('SELECT WEEK(t.CreatedAt) + 1 AS Week, tu.UserId, COUNT(tu.UserId) AS Count FROM vrp_ticket_users tu INNER JOIN vrp_tickets t ON t.Id = tu.TicketId WHERE t.AssigneeId IS NOT NULL AND t.AssigneeId <> tu.UserId AND t.State = \'Closed\' AND t.CreatedAt >= DATE_SUB(SUBDATE(NOW(), WEEKDAY(NOW())), INTERVAL ? WEEK) AND tu.IsAdmin = 1 AND tu.LeftAt IS NULL GROUP BY Week, UserId;', [$weeks]);
 
-        $result = [];
 
         $firstWeekCarbon = Carbon::now()->startOfWeek()->subWeeks($weeks);
         $endWeekCarbon = Carbon::now();
-        $firstWeek = $firstWeekCarbon->week;
+
+        $stats = DB::select('SELECT Id, ResolvedAt, AssigneeId FROM vrp_tickets WHERE AssigneeId IS NOT NULL AND State = \'Closed\' AND ResolvedAt >= ?', [$firstWeekCarbon]);
+        $stats2 = DB::select('SELECT tu.UserId, t.ResolvedAt, tu.UserId FROM vrp_ticket_users tu INNER JOIN vrp_tickets t ON t.Id = tu.TicketId WHERE t.AssigneeId IS NOT NULL AND t.AssigneeId <> tu.UserId AND t.State = \'Closed\' AND t.ResolvedAt >= ? AND tu.IsAdmin = 1 AND tu.LeftAt IS NULL', [$firstWeekCarbon]);
+
+        $result = [];
+
+        $firstWeek = $firstWeekCarbon->weekOfYear;
+
+        $lastYearTotalWeeks = -1;
+        if ($firstWeekCarbon->year != $endWeekCarbon->year)
+        {
+            // $lastYearTotalWeeks = (new Carbon('last day of December ' . $firstWeekCarbon->year, 'Europe/Vienna'))->weekOfYear;
+            $lastYearTotalWeeks = Carbon::parse('last day of December ' . $firstWeekCarbon->year, 'Europe/Vienna')->weekOfYear;
+        }
+
+        // KW 53
+        // KW 31 bis KW 4
         // $endWeek = $endWeekCarbon->week;
 
         foreach($members as $member)
@@ -41,49 +56,69 @@ class TicketController extends Controller
             ];
 
 
-            for($i = 0; $i < $firstWeekCarbon->diffInWeeks($endWeekCarbon); $i++) {
+            for($i = 0; $i <= $weeks; $i++) {
                 $week = $firstWeek + $i;
-                if ($week > 52)
-                    $week -= 52;
+                if ($week > $lastYearTotalWeeks)
+                    $week -= $lastYearTotalWeeks;
                 array_push($data['data'], [
                     'Week' => $week,
                     'ResolvedCount' => 0,
                     'ConsultedCount' => 0
                 ]);
             }
-
-
-            foreach($stats as $stat)
-            {
-                if($stat->AssigneeId === $member->Id)
-                {
-                    foreach($data['data'] as $key => $tmp)
-                    {
-                        if($tmp['Week'] === $stat->Week)
-                        {
-                            $data['data'][$key]['ResolvedCount'] = $stat->Count;
-                        }
-                    }
-                }
-            }
-
-
-            foreach($stats2 as $stat)
-            {
-                if($stat->UserId === $member->Id)
-                {
-                    foreach($data['data'] as $key => $tmp)
-                    {
-                        if($tmp['Week'] === $stat->Week)
-                        {
-                            $data['data'][$key]['ConsultedCount'] = $stat->Count;
-                        }
-                    }
-                }
-            }
-
+/*
+            dump($firstWeekCarbon->weekOfYear);
+            dump($endWeekCarbon->weekOfYear);
+            dd($data['data']);
+*/
             array_push($result, $data);
         }
+
+        foreach($stats as $stat)
+        {
+            $week = Carbon::parse($stat->ResolvedAt)->weekOfYear;
+
+            foreach($result as $key => $entry)
+            {
+                if ($entry['Id'] === $stat->AssigneeId) {
+
+                    foreach($result[$key]['data'] as $key2 => $tmp)
+                    {
+                        if($tmp['Week'] === $week)
+                        {
+                            $result[$key]['data'][$key2]['ResolvedCount'] += 1;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        foreach($stats2 as $stat)
+        {
+            $week = Carbon::parse($stat->ResolvedAt)->weekOfYear;
+
+
+
+            foreach($result as $key => $entry)
+            {
+                if ($entry['Id'] === $stat->UserId) {
+
+                    foreach($result[$key]['data'] as $key2 => $tmp)
+                    {
+                        if($tmp['Week'] === $week)
+                        {
+                            $result[$key]['data'][$key2]['ConsultedCount'] += 1;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+
 
         usort($result, function($a, $b) {
            return $a['Rank'] > $b['Rank'];
