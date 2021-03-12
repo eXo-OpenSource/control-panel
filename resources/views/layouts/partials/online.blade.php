917a6a0e60d0be8dc838f4913d9e8ffb0742b779
MegaThorx <git@merx.dev>
2021-03-12 11:15:28 +0100

    Fix online count

--- resources/views/layouts/partials/online.blade.php
+++ resources/views/layouts/partials/online.blade.php
@@ -1,4 +1,12 @@
 @php
     $usersOnline = \Illuminate\Support\Facades\Cache::get('users-online');
+
+    $count = 0;
+
+    try {
+        $count = count($usersOnline)
+    } catch (Exception $e) {
+
+    }
 @endphp
-<react-users-online data-children="{{ __(':online Benutzer online', ['online' => count($usersOnline)]) }}"></react-users-online>
+<react-users-online data-children="{{ __(':online Benutzer online', ['online' => $count]) }}"></react-users-online>
